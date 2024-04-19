4eb6d7edd714abb69fedeca6b81a0b87fa2dd8f2
Jamie Li <lijamie@gmail.com>
2024-04-19 10:06:38 -0700

    Add more vuls

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,24 @@
-plugins {
-    id("java")
-}
+plugins { id("java") }
 
 group = "org.stellar"
+
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
 dependencies {
-    // add snakeyaml dependency
-    implementation("org.yaml:snakeyaml:1.33")
-    // add postgres
-    implementation("org.postgresql:postgresql:42.7.3")
-    // add json dependency
-    implementation("org.json:json:20201115 ")
+  // add snakeyaml dependency
+  implementation("org.yaml:snakeyaml:1.33")
+  // add postgres
+  implementation("org.postgresql:postgresql:42.7.1")
+  // add json dependency
+  implementation("org.json:json:20201115")
+  // add snappy-java
+  implementation("org.xerial.snappy:snappy-java:1.1.8.4")
+  implementation("org.xerial:sqlite-jdbc:3.36.0.3")
 
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+  testImplementation(platform("org.junit:junit-bom:5.10.0"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+tasks.test { useJUnitPlatform() }
--- src/main/java/org/stellar/Main.java
+++ src/main/java/org/stellar/Main.java
@@ -2,10 +2,33 @@ package org.stellar;
 
 import org.yaml.snakeyaml.Yaml;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws SQLException {
+    testYaml();
+    testJdbc();
     Yaml yaml = new Yaml();
     String document = "hello: world 2";
+    Object data = yaml.load(document);
+    System.out.println(data);
+  }
+
+  private static void testJdbc() throws SQLException {
+    String url = "jdbc:postgresql://localhost/test";
+    Properties props = new Properties();
+    props.setProperty("user", "fred");
+    props.setProperty("password", "secret");
+    props.setProperty("ssl", "true");
+    DriverManager.getConnection(url, props);
+  }
+
+  private static void testYaml() {
+    Yaml yaml = new Yaml();
+    String document = "hello: world";
     Object data = yaml.load(document);
     System.out.println(data);
   }
